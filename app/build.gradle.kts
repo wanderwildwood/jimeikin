4431f424c07672248c26fe774999b038de193ae3
wander wildwood <wanderwildwood@users.noreply.github.com>
2026-09-13 13:57:45 -0400

    Version 2.8.13

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.wanderwildwood.jimeikin"
         minSdk = 28
         targetSdk = 36
-        versionCode = 33
-        versionName = "2.8.12"
+        versionCode = 34
+        versionName = "2.8.13"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
